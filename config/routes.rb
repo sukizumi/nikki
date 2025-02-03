2ef7e0f6a2d60106dd6de7c7cdc09437f488326a
sukizumi <187249056+sukizumi@users.noreply.github.com>
2025-02-03 10:38:15 +0000

    top

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  scope module: :public do
+    root "homes#top"
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
