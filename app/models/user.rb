eb808d81f3e89146147a3af2cc6f323894d0fe72
sukizumi <187249056+sukizumi@users.noreply.github.com>
2025-02-09 06:03:03 +0000

    [fixed] enum

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,34 +8,13 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_one_attached :profile_image
 
-  validates :name, uniqueness: true
+  validates :name, presence: true, uniqueness: true
+  validates :gender, presence: true
+  validates :height, presence: true
+  validates :weight, presence: true
 
   enum gender: { male: 1, female: 2, blank:0 }
 
-  def BMI_calculation
-    latest_post = posts.last
-    return nil unless lastest_post&.weight && height
-
-    latest_post.weight.to_f / ((height.to_f / 100) ** 2)
-  end
-
-  def BMI_rate
-    return "データ不足" unless BMI_calculation
-
-    case BMI_calculation
-    when 0..18.5
-      "低体重"
-    when 18.5..24.9
-      "普通体重"
-    when 25..29.9
-      "肥満(1度)"
-    when 30..34.9
-      "肥満(2度)"
-    when 35..39.9
-      "肥満(3度)"
-    else
-      "肥満(4度)"
-    end
-  end
+  
 
 end
