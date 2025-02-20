24439ead792859bfa538f28b7a7227e3ba690f73
sukizumi <187249056+sukizumi@users.noreply.github.com>
2025-02-20 10:23:59 +0000

    likes index fix

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -35,7 +35,6 @@ class Public::UsersController < ApplicationController
     @user = User.find(params[:id])
     likes = Like.where(user_id: @user.id).pluck(:post_id)
     @liked_posts = Post.find(likes)
-    @post = Post.find(params[:id])
   end
 
   def following
